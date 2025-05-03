2e0a41e0ca33ba10f01043b12c698b3836dae333
jeff <jefflining@gmail.com>
2025-05-02 20:12:14 -0500

    push

--- shopping-cart/cart.js
+++ shopping-cart/cart.js
@@ -1,3 +1,15 @@
+let dummieCart = [];
+
+let removeCart = [{ item: 'apple', quantity: 1 }, { item: 'banana', quantity: 1}, { item: 'orange', quantity: 1} ]
+
+let getTotalItemsCart = [{ item: 'apple', quantity: 1 }, { item: 'banana', quantity: 1}, { item: 'orange', quantity: 1} ];
+
+let emptyCart = [];
+
+let highQuantityCart = [{ item: 'apple', quantity: 2000 }, { item: 'banana', quantity: 2000}, { item: 'orange', quantity: 1000}];
+
+
+
 function addItem(cart, item, quantity) {
   if (quantity < 0) {
         return "Error: quantity cannot be negative.";
@@ -8,21 +20,26 @@ function addItem(cart, item, quantity) {
   }
   return cart;
 }
-  function removeItem(cart, item) {
-      const index = cart.findIndex(cartItem => cartItem.item === item);
-      if (index === -1) {
-          return "Error: This item is not in cart.  Item cannot be removed"
+
+
+
+function removeItem(cart, item) {
+    const index = cart.findIndex(cartItem => cartItem.item === item);
+    if (index === -1) {
+        return "Error: This item is not in cart.  Item cannot be removed"
+    }
+    if (index !== -1) {
+      if (cart[index].quantity > 1) {
+        cart[index].quantity -= 1;
+      } else {
+          
+        cart.splice(index, 1);
       }
-      if (index !== -1) {
-        if (cart[index].quantity > 1) {
-          cart[index].quantity -= 1;
-        } else {
-          // Remove item completely if quantity is 1
-          cart.splice(index, 1);
-        }
-      }
-      return cart;
+    }
+    return cart;
 }
+
+
 
 function getTotalItems(cart) {
 return cart.reduce (function (total, item) {
@@ -31,17 +48,13 @@ return cart.reduce (function (total, item) {
 }
 
 
-  // console.log(cart.reduce(removeItem(total, cart.quantity, 0)))
-  //     return total + quantity;
-  // }
 
-//let dummieCart = [{ item: 'apple', quantity: 1 }, { item: 'banana', quantity: 1}, { item: 'orange', quantity: 1} ];
-let dummieCart = [];
-// console.log(addItem(dummieCart, "apple", 1));
-
-// console.log(removeItem(dummieCart,"carrot"));
-//console.log(dummieCart);
-console.log(getTotalItems(dummieCart));
-// (dummieCart);
-
-module.exports = {addItem, removeItem, getTotalItems,};
+module.exports = {addItem, 
+  removeItem, 
+  getTotalItems, 
+  dummieCart: dummieCart,
+   removeCart: removeCart,
+  getTotalItemsCart: getTotalItemsCart,
+  emptyCart: emptyCart,
+  highQuantityCart: highQuantityCart,
+};
--- tests/cart.test.js
+++ tests/cart.test.js
@@ -1,15 +1,13 @@
 const cart = require('../shopping-cart/cart');
 
-//console.log(cart.addItem(dummieCart,"apple",2));
-
 describe('The addItem function', () => {
-    let dummieCart = []
+    
     // POSITIVE CASE
 
     // EXAMPLE: Write a test for addItem
 
     test('should log item and quantity added to cart', () => {
-        expect(cart.addItem(dummieCart, "apple", 2)).toEqual([{ item: "apple", quantity: 2 }]);
+        expect(cart.addItem(cart.dummieCart, "apple", 2)).toEqual([{ item: "apple", quantity: 2 }]);
     });
 
      // NEGATIVE CASE
@@ -17,7 +15,7 @@ describe('The addItem function', () => {
     // EXAMPLE: Write a test for addItem
 
     test('should log error message when a negtive quanity is added to the cart', () => {
-        expect(cart.addItem(dummieCart, "apple", -2)).toBe("Error: quantity cannot be negative.");
+        expect(cart.addItem(cart.dummieCart, "apple", -2)).toBe("Error: quantity cannot be negative.");
 
     });
 
@@ -26,28 +24,28 @@ describe('The addItem function', () => {
     // EXAMPLE: Write a test for addItem
 
     test('should log error message when an item with a quantity of zero is entered', () => {
-        expect(cart.addItem(dummieCart, "apple", 0)).toBe("Error: quantity cannot equal to zero.");
+        expect(cart.addItem(cart.dummieCart, "apple", 0)).toBe("Error: quantity cannot equal to zero.");
 
     });
 
 });
 
 describe('The removeItem function', () => {
-    let dummieCart = [{ item: 'apple', quantity: 1 }, { item: 'banana', quantity: 1}, { item: 'orange', quantity: 1} ];
+  
     // POSITIVE CASE
 
     // EXAMPLE: Write a test for removeItem
 
     test('should remove the specified item from the cart and log the updated cart', () => {
-        expect(cart.removeItem(dummieCart, "apple")).toEqual([{ item: 'banana', quantity: 1}, { item: 'orange', quantity: 1}]);
+        expect(cart.removeItem(cart.removeCart, "apple")).toEqual([{ item: 'banana', quantity: 1}, { item: 'orange', quantity: 1}]);
     });
 
     // NEGATIVE CASE
 
     // EXAMPLE: Write a test for removeItem
 
-    test('should remove the specified item from the cart and log the updated cart', () => {
-        expect(cart.removeItem(dummieCart, "carrot")).toBe("Error: This item is not in cart.  Item cannot be removed");
+    test('when an attempt is made to remove an item that is not in cart, an error indicating this will be logged', () => {
+        expect(cart.removeItem(cart.removeCart, "carrot")).toBe("Error: This item is not in cart.  Item cannot be removed");
     });
 
     // EDGE CASE
@@ -55,27 +53,32 @@ describe('The removeItem function', () => {
     // EXAMPLE: Write a test for removeItem
 
     test('should remove the last item in the cart and log updated cart', () => {
-        expect(cart.removeItem(dummieCart, "orange")).toEqual([{ item: 'banana', quantity: 1}]);
+        expect(cart.removeItem(cart.removeCart, "orange")).toEqual([{ item: 'banana', quantity: 1}]);
     });
 
 });
 
     describe('The getTotalItems function', () => {
-        let dummieCart = [{ item: 'apple', quantity: 1 }, { item: 'banana', quantity: 1}, { item: 'orange', quantity: 1} ];
+        
         // POSITIVE CASE
     
         // EXAMPLE: Write a test for getTotalItems
     
-        test('should log number of items in cart', () => {
-            expect(cart.getTotalItems(dummieCart)).toEqual(3);
-        });
+    test('should log number of items in cart', () => {
+        expect(cart.getTotalItems(cart.getTotalItemsCart)).toEqual(3);
+    });
 
-        // NEGATIVE CASE
-    
-        // EXAMPLE: Write a test for getTotalItems
-    
-        test('should log number of items in cart', () => {
-            expect(cart.getTotalItems(dummieCart)).toEqual();
+    // NEGATIVE CASE
 
-        });
+    // EXAMPLE: Write a test for getTotalItems
+
+    test('should log 0 for an empty cart', () => {
+        expect(cart.getTotalItems(cart.emptyCart)).toEqual(0);
+
+    });
+
+    test('should log 5000 for highQuantityCart with a quantity 5000 items', () => {
+        expect(cart.getTotalItems(cart.highQuantityCart)).toEqual(5000);
+
+    });
 });
